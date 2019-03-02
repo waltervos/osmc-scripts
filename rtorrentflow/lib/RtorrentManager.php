169ad69c6e9c95c73ad8d0ea9e6b68f0e6ff2121
Walter Vos <wthvos@gmail.com>
2019-03-02 22:02:20 +0100

    Might have made a typo in some changes I did to RtorrentClient::LoadMethod() in an earlier commit.

--- rtorrentflow/lib/RtorrentManager.php
+++ rtorrentflow/lib/RtorrentManager.php
@@ -146,7 +146,9 @@ class RtorrentManager {
                                 'd.custom2.set=' . $custom2,
                                 'd.custom3.set=' . $queued_torrent['custom3'],
                                 'd.throttle_name.set=' . $throttle,
-                                'view.set_visible=' . $view, $this->load_method
+                                'view.set_visible=' . $view
+                            ),
+                            $this->load_method
                             )
                         )) { // ... if($this->rtorrent_client->loadTorrent()):
                         Log::addMessage("Torrent " . $queued_torrent['tied_to_file'] . " loaded.", 'info');
