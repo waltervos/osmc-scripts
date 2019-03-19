ced3d1f45695a7f791d5bc06c7ccfc310e68c354
Walter Vos <wthvos@gmail.com>
2019-03-19 21:49:16 +0100

    Fixed file formatting of Log.php (set it as Unix style)

--- rtorrentflow/lib/Log.php
+++ rtorrentflow/lib/Log.php
@@ -1,1 +1,22 @@
-<?phpclass Log {    public static $messages = array();    public static function init() {    }    public static function addMessage($content, $level) {        self::$messages[$level][] = array('timestamp' => new DateTime('now'), 'content' => $content);    }    public static function printMessages($loglevel) {        $format = '%1s: %2s %3s' . "\n";        foreach (self::$messages as $level => $messages) {            if ($loglevel !== 'debug' && $level !== $loglevel) continue;            foreach ($messages as $message) {                printf($format, $message['timestamp']->format('d-m-Y H:i:s') , $level, $message['content']);            }        }    }}?>
+<?php
+class Log {
+    public static $messages = array();
+
+    public static function init() {
+    }
+
+    public static function addMessage($content, $level) {
+        self::$messages[$level][] = array('timestamp' => new DateTime('now'), 'content' => $content);
+    }
+
+    public static function printMessages($loglevel) {
+        $format = '%1s: %2s %3s' . "\n";
+        foreach (self::$messages as $level => $messages) {
+            if ($loglevel !== 'debug' && $level !== $loglevel) continue;
+            foreach ($messages as $message) {
+                printf($format, $message['timestamp']->format('d-m-Y H:i:s') , $level, $message['content']);
+            }
+        }
+    }
+}
+?>
